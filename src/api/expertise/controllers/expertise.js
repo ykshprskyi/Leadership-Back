9e902b312ed61523de9f2ab5fca4aeb897c9123d
ykshprskyi <ykashperskiy162@gmail.com>
2023-11-16 18:44:41 +0200

    leadership back

---
+++ src/api/expertise/controllers/expertise.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * expertise controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::expertise.expertise');
---
+++ src/api/expertise/services/expertise.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * expertise service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::expertise.expertise');
